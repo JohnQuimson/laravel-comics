1fd7447a80b632f3c0a472a9fe7d9b9ffe0fc839
JohnQuimson <john.quimson777@gmail.com>
2024-01-27 12:51:12 +0100

    feat: header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -7,7 +7,56 @@
       </ul>
     </div>
   </div>
-
+  <div class="navbar">
+      <ul class="container d-flex justify-content-between align-items-center text-uppercase">
+      <li>
+        <div class="cont-logo">
+             <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo"> 
+        </div>
+      </li>
+      <li>
+        <a href="#" class="selected">characters</a>
+      </li>
+      <li>
+        <a href="#">comics</a>
+      </li>
+      <li>
+        <a href="#">movies</a>
+      </li>
+      <li>
+        <a href="#">tv</a>
+      </li>
+      <li>
+        <a href="#">games</a>
+      </li>
+      <li>
+        <a href="#">collectibles</a>
+      </li>
+      <li>
+        <a href="#">videos</a>
+      </li>
+      <li>
+        <a href="#">fans</a>
+      </li>
+      <li>
+        <a href="#">news</a>
+      </li>
+      <li>
+        <a href="#">shop</a>
+      </li>
+      <li>
+        <input type="search" placeholder="Search">
+        <button>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </li>
+    </ul>
+  </div>
+  <section class="jumbotron">
+    <div class="cont-jumbo">
+      <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="jumbotron"> 
+    </div>
+  </section>
 
 
 
